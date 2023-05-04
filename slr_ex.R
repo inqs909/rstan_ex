ac0af18a87c2c842b6dd2f3d57a64935834bb0cf
Isaac Quintanilla <inqs.909@gmail.com>
2023-05-04 08:14:48 -0700

    Small Updates

--- slr_ex.R
+++ slr_ex.R
@@ -8,6 +8,7 @@ x <- rnorm(1000, 4)
 y <- 4 - 3 * x + rnorm(1000, sd = 2)
 data <- list(N = length(x), X = x, Y = y)
 res <- model$sample(data = data, seed = 655,
-                    chains = 6,
+                    chains = 6, iter_warmup = 2000,
+                    iter_sampling = 3000
                     )
 res_sum <- res$summary()
