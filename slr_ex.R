c9e95a6d108b5c4d4e0944bd6e6b2a79013a7f92
Isaac Quintanilla <inqs.909@gmail.com>
2023-05-04 08:05:10 -0700

    Initial Commit

---
+++ slr_ex.R
@@ -0,0 +1,13 @@
+library(cmdstanr)
+model <- cmdstan_model("slr_ex.stan")
+
+
+
+
+x <- rnorm(1000, 4)
+y <- 4 - 3 * x + rnorm(1000, sd = 2)
+data <- list(N = length(x), X = x, Y = y)
+res <- model$sample(data = data, seed = 655,
+                    chains = 6,
+                    )
+res_sum <- res$summary()
